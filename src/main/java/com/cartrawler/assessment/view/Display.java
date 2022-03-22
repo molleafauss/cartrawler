86ef3a820d77c0b0cc095508453119cf4ea6ecda
molleafauss <lmollea@yahoo.it>
2022-03-22 15:59:20 +0100

    Initial import

---
+++ src/main/java/com/cartrawler/assessment/view/Display.java
@@ -0,0 +1,13 @@
+package com.cartrawler.assessment.view;
+
+import com.cartrawler.assessment.car.CarResult;
+
+import java.util.Set;
+
+public class Display {
+    public void render(Set<CarResult> cars) {
+        for (CarResult car : cars) {
+            System.out.println (car);
+        }
+    }
+}
