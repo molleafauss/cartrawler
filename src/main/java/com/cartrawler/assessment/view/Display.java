72f4f92b9b736ce0f93930fc2c82105d99a364f1
molleafauss <lmollea@yahoo.it>
2022-03-23 15:54:34 +0100

    removed set, better output

--- src/main/java/com/cartrawler/assessment/view/Display.java
+++ src/main/java/com/cartrawler/assessment/view/Display.java
@@ -6,8 +6,11 @@ import java.util.Set;
 
 public class Display {
     public void render(Set<CarResult> cars) {
+        System.out.println(" SUPPLIER  | MAKE / MODEL                                       | SIPP |  PRICE   | FUEL");
+        System.out.println("-----------+----------------------------------------------------+------+----------+-----------");
         for (CarResult car : cars) {
-            System.out.println (car);
+            System.out.printf("%-10.10s | %-50.50s | %4.4s | %8.2f | %-9.9s %n", car.getSupplierName(), car.getDescription(),
+                    car.getSippCode(), car.getRentalCost(), car.getFuelPolicy());
         }
     }
 }
