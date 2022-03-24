778a0adf3532feb22e431b95d3469a3e4de1f5c8
molleafauss <lmollea@yahoo.it>
2022-03-24 15:19:19 +0100

    Moved loader to its class

--- src/main/java/com/cartrawler/assessment/app/AssessmentRunner.java
+++ src/main/java/com/cartrawler/assessment/app/AssessmentRunner.java
@@ -1,53 +1,19 @@
 package com.cartrawler.assessment.app;
 
-import com.cartrawler.assessment.car.CarResult;
+import com.cartrawler.assessment.loader.CarResultLoader;
 import com.cartrawler.assessment.service.FilterService;
 import com.cartrawler.assessment.view.Display;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.commons.csv.CSVFormat;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.HashSet;
-import java.util.Set;
 
 public class AssessmentRunner {
 
-    static final Logger LOGGER = LoggerFactory.getLogger(AssessmentRunner.class);
-
     public static final String DEFAULT_FILE_NAME = "cars.csv";
-    public static final String[] CSV_HEADERS = {"DESCRIPTION", "SUPPLIER", "SIPP", "PRICE", "FUEL_POLICY"};
 
     public static void main(String[] args) throws IOException {
-        var cars = loadCsv();
+        var cars = new CarResultLoader().loadCsv(DEFAULT_FILE_NAME);
         var filterService = new FilterService();
         Display display = new Display();
         display.render(filterService.filter(cars));
-    }
-
-    private static Set<CarResult> loadCsv() throws IOException {
-        var reader = new FileReader(DEFAULT_FILE_NAME, StandardCharsets.UTF_8);
-        var csv = CSVFormat.Builder.create()
-                .setHeader(CSV_HEADERS)
-                .setSkipHeaderRecord(true)
-                .build()
-                .parse(reader);
-        var cars = new HashSet<CarResult>();
-        csv.forEach(record -> {
-            var car = new CarResult(
-                    record.get(CSV_HEADERS[0]),
-                    record.get(CSV_HEADERS[1]),
-                    record.get(CSV_HEADERS[2]),
-                    Double.parseDouble(record.get(CSV_HEADERS[3])),
-                    CarResult.FuelPolicy.valueOf(record.get(CSV_HEADERS[4]))
-            );
-            if (!cars.add(car)) {
-                LOGGER.warn("Found duplicate result at row {} - {}", csv.getCurrentLineNumber(), car);
-            }
-        });
-        LOGGER.info("Loaded {} cars from {} [{} lines]", cars.size(), DEFAULT_FILE_NAME, csv.getRecordNumber());
-        return cars;
     }
 }
---
+++ src/main/java/com/cartrawler/assessment/loader/CarResultLoader.java
@@ -0,0 +1,44 @@
+package com.cartrawler.assessment.loader;
+
+import com.cartrawler.assessment.car.CarResult;
+import org.apache.commons.csv.CSVFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Loads data from the given file
+ */
+public class CarResultLoader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CarResultLoader.class);
+    private static final String[] CSV_HEADERS = {"DESCRIPTION", "SUPPLIER", "SIPP", "PRICE", "FUEL_POLICY"};
+
+    public Set<CarResult> loadCsv(String fileName) throws IOException {
+        var reader = new FileReader(fileName, StandardCharsets.UTF_8);
+        var csv = CSVFormat.Builder.create()
+                .setHeader(CSV_HEADERS)
+                .setSkipHeaderRecord(true)
+                .build()
+                .parse(reader);
+        var cars = new HashSet<CarResult>();
+        csv.forEach(record -> {
+            var car = new CarResult(
+                    record.get(CSV_HEADERS[0]),
+                    record.get(CSV_HEADERS[1]),
+                    record.get(CSV_HEADERS[2]),
+                    Double.parseDouble(record.get(CSV_HEADERS[3])),
+                    CarResult.FuelPolicy.valueOf(record.get(CSV_HEADERS[4]))
+            );
+            if (!cars.add(car)) {
+                LOGGER.warn("Found duplicate result at row {} - {}", csv.getCurrentLineNumber(), car);
+            }
+        });
+        LOGGER.info("Loaded {} cars from {} [{} lines]", cars.size(), fileName, csv.getRecordNumber());
+        return cars;
+    }
+}
